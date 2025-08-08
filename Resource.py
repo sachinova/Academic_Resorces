6fb13b930937339d17d240206fd93c52a867e083
Sachin Singh Rautela <rautelasachin2007@gmail.com>
2025-08-08 18:27:55 +0530

    Update Resource.py

--- Resource.py
+++ Resource.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 st.set_page_config(page_title="Student Resource Portal", layout="wide")
-st.markdown("### Welcome to the valley of Academic Treasure")
+#st.markdown("### Welcome to the valley of Academic Treasure")
 
 
 st.markdown("<br><br><br>", unsafe_allow_html=True)
